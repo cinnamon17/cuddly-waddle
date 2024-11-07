911af3b65a65a16c4b4a25ee3330614663353058
Nelson Moncada <juniormoncada17@gmail.com>
2024-11-07 20:00:38 +0100

    BoyOrGirl better performance

--- BoyOrGirl.java
+++ BoyOrGirl.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -8,20 +9,17 @@ public class BoyOrGirl {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		String row = scanner.nextLine();
-		StringBuffer str = new StringBuffer();
+		HashMap<Character, Integer> chars = new HashMap<>();
 		for (int i = 0; i < row.length(); i++) {
-			if (str.indexOf(String.valueOf(row.charAt(i))) == -1) {
-				str.append(row.charAt(i));
-			}
+			chars.put(row.charAt(i), 1);
 		}
 
-		if (str.length() % 2 == 0) {
+		if (chars.size() % 2 == 0) {
 
 			System.out.println("CHAT WITH HER!");
 		} else {
 			System.out.println("IGNORE HIM!");
 		}
 		scanner.close();
-
 	}
 }
